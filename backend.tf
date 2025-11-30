cb2fb8f4a209d9196555750db8f84c3d263bf895
ironwithout <lupus.vertex@gmail.com>
2025-11-30 14:17:07 +0100

    fix: remove unneeded blanks

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-436204347828-us-east-1"
-    key            = "aws-iac/ecs-webapp/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
+    bucket       = "terraform-state-436204347828-us-east-1"
+    key          = "aws-iac/ecs-webapp/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
