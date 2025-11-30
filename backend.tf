60afea1dd4ad273b1ff87fada3f06b1388d3acb0
Miguel Sánchez <lupus.vertex@gmail.com>
2025-11-30 14:18:24 +0100

    feat: setup remote state (s3) (#3)

--- backend.tf
+++ backend.tf
@@ -1,16 +1,8 @@
-# Backend configuration for Terraform state
-# Note: Backend resources (S3 bucket + DynamoDB table) must be bootstrapped manually
-# before first `terraform init`.
-#
-# Uncomment and configure after creating backend resources:
-#
-# terraform {
-#   backend "s3" {
-#     bucket         = "your-terraform-state-bucket"
-#     key            = "aws-iac/ecs-webapp/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     kms_key_id     = "alias/terraform-state"
-#     dynamodb_table = "terraform-state-lock"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket       = "terraform-state-436204347828-us-east-1"
+    key          = "aws-iac/ecs-webapp/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
